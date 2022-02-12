ffa4fd6e275ecfc6ff882d62f7f389e6dc767de7
Oasiris <teracontinens@gmail.com>
2022-02-11 20:49:53 -0800

    Remove About page for first draft of site

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -9,7 +9,8 @@ import Header from './header'
 const App: FunctionalComponent = () => {
     return (
         <div id="root">
-            <Header />
+            {/* <Header /> */}
+            <div style={{ height: '40px' }} />
             <Router>
                 <Route path="/" component={Home} />
                 {/* <Route path="/about/" component={About} user="me" /> */}
